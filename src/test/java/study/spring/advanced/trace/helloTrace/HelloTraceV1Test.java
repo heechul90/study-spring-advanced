aa9587d68d3ebefd43ac7a23321b93d9f56fb348
hclee <heechul4296@gmail.com>
2022-03-12 11:42:37 +0900

    로그 추적기 v1 - 프로토타입 개발

---
+++ src/test/java/study/spring/advanced/trace/helloTrace/HelloTraceV1Test.java
@@ -0,0 +1,27 @@
+package study.spring.advanced.trace.helloTrace;
+
+import org.junit.jupiter.api.Test;
+import study.spring.advanced.trace.TraceStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+    @Test
+    void exception() {
+    }
+}
