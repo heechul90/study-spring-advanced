ec2e3928a95a5869b917143af557fd3eb22427ea
hclee <heechul4296@gmail.com>
2022-03-13 14:30:32 +0900

    로그 추적기 v1 - 적용

---
+++ src/main/java/study/spring/advanced/app/v1/OrderServiceV1.java
@@ -0,0 +1,28 @@
+package study.spring.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.spring.advanced.trace.TraceStatus;
+import study.spring.advanced.trace.helloTrace.HelloTraceV1;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
