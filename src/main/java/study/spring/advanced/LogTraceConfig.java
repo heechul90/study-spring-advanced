e7ed3ad5a0b4a0b465a83f9672dc119fe34d2842
hclee <heechul4296@gmail.com>
2022-03-13 17:53:47 +0900

    ThreadLocal 동기화 - 적용

--- src/main/java/study/spring/advanced/LogTraceConfig.java
+++ src/main/java/study/spring/advanced/LogTraceConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import study.spring.advanced.trace.logtrace.FieldLogTrace;
 import study.spring.advanced.trace.logtrace.LogTrace;
+import study.spring.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        //return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
